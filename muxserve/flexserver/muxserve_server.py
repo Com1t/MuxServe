9ddb132db18679a4fb8b08fd3ffc6032be384dca
Fu-Chiang Chang <fuchiang137@gmail.com>
2025-09-16 17:05:14 +0800

    Removed workload parameter for LLM runtime and used formatter

--- muxserve/flexserver/muxserve_server.py
+++ muxserve/flexserver/muxserve_server.py
@@ -15,27 +15,32 @@ from vllm.zmq_tool import ZMQClient
 
 from typing import List, Optional, Union
 
-from muxserve.constants import (SM_HOLD_NAME_FMT, ADD_REQ_NAME_FMT,
-                              RET_REQ_NAME_FMT, PREEMPT_REQ_NAME_FMT)
-from muxserve.flexserver.dist_utils import (_init_distributed_environment,
-                                          _setup_env)
+from muxserve.constants import (
+    SM_HOLD_NAME_FMT,
+    ADD_REQ_NAME_FMT,
+    RET_REQ_NAME_FMT,
+    PREEMPT_REQ_NAME_FMT,
+)
+from muxserve.flexserver.dist_utils import _init_distributed_environment, _setup_env
 from muxserve.flexserver.llm_runtime import LLMRuntime
 from muxserve.muxsched.workload_utils import Workload, Request
-from muxserve.shm_utils import (create_shared_var, read_shared_var,
-                              write_shared_var, dump_to_shared_var,
-                              load_from_shared_var, load_reqs_from_shared_var,
-                              dump_reqs_to_shared_var)
+from muxserve.shm_utils import (
+    create_shared_var,
+    read_shared_var,
+    write_shared_var,
+    dump_to_shared_var,
+    load_from_shared_var,
+    load_reqs_from_shared_var,
+    dump_reqs_to_shared_var,
+)
 from muxserve.logger import init_logger
 
 logger = init_logger("MuxServe")
 
 
 class MuxServeEngine:
-
-    def __init__(self, llm_runtime: LLMRuntime, workload: Workload,
-                 mps_percentage: int, is_prefill: bool):
+    def __init__(self, llm_runtime: LLMRuntime, mps_percentage: int, is_prefill: bool):
         self.llm_runtime = llm_runtime
-        self.workload = workload
         self.mps_percentage = mps_percentage
         self.is_prefill = is_prefill
         self.model_name = self.llm_runtime.model_name
@@ -48,16 +53,19 @@ class MuxServeEngine:
 
         # Create shared variables.
         if self.llm_runtime.tcp_client is not None:
-            sm_hold_shm_name = SM_HOLD_NAME_FMT.format(self.model_name,
-                                                       self.mps_percentage)
-            self.sm_hold_shm = create_shared_var(sm_hold_shm_name,
-                                                 create=False)
+            sm_hold_shm_name = SM_HOLD_NAME_FMT.format(
+                self.model_name, self.mps_percentage
+            )
+            self.sm_hold_shm = create_shared_var(sm_hold_shm_name, create=False)
             self.add_req_shm_name = ADD_REQ_NAME_FMT.format(
-                self.model_name, self.mps_percentage)
+                self.model_name, self.mps_percentage
+            )
             self.ret_req_shm_name = RET_REQ_NAME_FMT.format(
-                self.model_name, self.mps_percentage)
+                self.model_name, self.mps_percentage
+            )
             self.preempt_req_shm_name = PREEMPT_REQ_NAME_FMT.format(
-                self.model_name, self.mps_percentage)
+                self.model_name, self.mps_percentage
+            )
         self.requests_in_queue: Set[int] = set()
         self.requests_max_tokens: Dict[int, int] = {}
 
@@ -67,13 +75,18 @@ class MuxServeEngine:
         self.start_batch = 100
         self.stop_batch = 120 if self.is_prefill else 150
         self.cur_batch = 0
-        self.prof = torch.profiler.profile(
-            activities=[
-                torch.profiler.ProfilerActivity.CPU,
-                torch.profiler.ProfilerActivity.CUDA
-            ],
-            with_stack=True,
-            with_modules=True) if self.enable_profiler else None
+        self.prof = (
+            torch.profiler.profile(
+                activities=[
+                    torch.profiler.ProfilerActivity.CPU,
+                    torch.profiler.ProfilerActivity.CUDA,
+                ],
+                with_stack=True,
+                with_modules=True,
+            )
+            if self.enable_profiler
+            else None
+        )
         model = self.model_name.split("/")[-1]
         self.prof_out_name = f"log/profiler_muxserve/profiler_{model}_mps{self.mps_percentage}_w{self.world_size}_rank{self.rank}.json"
 
@@ -107,12 +120,14 @@ class MuxServeEngine:
 
             if self.is_prefill:
                 self.llm_runtime.batch_scheduler.add_request(
-                    prompt_tokens, req_id, output_len)
+                    prompt_tokens, req_id, output_len
+                )
                 self.requests_max_tokens[req_id] = max_tokens
             else:
                 if req_id not in self.requests_in_queue:
                     self.llm_runtime.batch_scheduler.add_request(
-                        prompt_tokens, req_id, output_len)
+                        prompt_tokens, req_id, output_len
+                    )
                     self.requests_in_queue.add(req_id)
                     self.requests_max_tokens[req_id] = max_tokens
                     # reset output idx for new requests
@@ -123,8 +138,7 @@ class MuxServeEngine:
         batch_output_tokens = batch_reqs[num_requests:]
         return batch_request_ids, batch_output_tokens
 
-    def process_output(self, outputs: List[int],
-                       batch_reqs: List[List[int]]) -> None:
+    def process_output(self, outputs: List[int], batch_reqs: List[List[int]]) -> None:
         # logger.info(
         #     f"Final stage outputs: {outputs}, batch_reqs: {batch_reqs}")
         generated = [req[0] for req in batch_reqs] + outputs
@@ -147,7 +161,8 @@ class MuxServeEngine:
                 batch_request_ids, batch_output_tokens = self.add_requests()
 
                 outputs, batch_reqs, preempted_reqs = self.llm_runtime.pipeline_step(
-                    batch_request_ids, batch_output_tokens)
+                    batch_request_ids, batch_output_tokens
+                )
                 exec_bs.append(len(batch_reqs))
                 if len(batch_reqs) > 0:
                     self.cur_batch += 1
@@ -161,7 +176,7 @@ class MuxServeEngine:
 
                 # free kv cache blocks for finished requests
                 free_request_ids, finished_request_ids = [], []
-                for (req_id, max_token) in batch_reqs:
+                for req_id, max_token in batch_reqs:
                     req = self.requests[req_id]
                     # req = self.llm_runtime.batch_scheduler._requests[] # need to move the definition of request from python to cpp
                     if req.output_idx == max_token - 1:
@@ -171,11 +186,12 @@ class MuxServeEngine:
                 self.requests_in_queue.difference_update(preempted_reqs)
                 if free_request_ids:
                     self.requests_in_queue.difference_update(free_request_ids)
-                    self.llm_runtime.release_request(free_request_ids,
-                                                     finished_request_ids)
+                    self.llm_runtime.release_request(
+                        free_request_ids, finished_request_ids
+                    )
                     # logger.info(
-                    #     f"Requests {free_request_ids} free, {finished_request_ids} finished, "
-                    #     f"KV cache are freed or stored!")
+                    #     f"Requests {free_request_ids} free, {finished_request_ids} finished!"
+                    # )
 
                 num_iters -= 1
 
@@ -184,8 +200,7 @@ class MuxServeEngine:
                         self.prof.start()
                     elif self.cur_batch == self.stop_batch:
                         self.prof.stop()
-                        logger.info(
-                            f"Export profiler trace to {self.prof_out_name}")
+                        logger.info(f"Export profiler trace to {self.prof_out_name}")
                         self.prof.export_chrome_trace(self.prof_out_name)
                         self.enable_profiler = False
 
@@ -203,7 +218,10 @@ class MuxServeEngine:
                 if self.cur_batch % 200 == 0:
                     self.memory_stats()
 
-            if self.llm_runtime.stage_id == self.llm_runtime.num_stages - 1 and self.llm_runtime.tp_rank == 0:
+            if (
+                self.llm_runtime.stage_id == self.llm_runtime.num_stages - 1
+                and self.llm_runtime.tp_rank == 0
+            ):
                 if len(batch_reqs) > 0:
                     self.process_output(outputs, batch_reqs)
 
@@ -212,8 +230,7 @@ class MuxServeEngine:
                     self.prof.start()
                 elif self.cur_batch == self.stop_batch:
                     self.prof.stop()
-                    logger.info(
-                        f"Export profiler trace to {self.prof_out_name}")
+                    logger.info(f"Export profiler trace to {self.prof_out_name}")
                     self.prof.export_chrome_trace(self.prof_out_name)
                     self.enable_profiler = False
 
@@ -222,8 +239,9 @@ class MuxServeEngine:
             f"MuxServe engine started (MPS: {self.mps_percentage})! "
             f"max_num_seqs: {self.llm_runtime.scheduler_config.max_num_seqs} "
             f"max_batched_token: "
-            f"{self.llm_runtime.scheduler_config.max_num_batched_tokens}")
-        self.memory_stats()
+            f"{self.llm_runtime.scheduler_config.max_num_batched_tokens}"
+        )
+        # self.memory_stats()
         if self.llm_runtime.stage_id == 0 and self.llm_runtime.tp_rank == 0:
             self.first_stage_loop()
         else:
@@ -234,11 +252,13 @@ class MuxServeEngine:
         allocated_memory = torch.cuda.memory_allocated() / 1024**3
         reserved_memory = torch.cuda.memory_reserved() / 1024**3
         max_reserved_memory = torch.cuda.max_memory_reserved() / 1024**3
-        logger.info(f"Rank {self.rank} Memory Stats: "
-                    f"Allocated {allocated_memory:.2f} GB, "
-                    f"Max Allocated {max_allocated_memory:.2f} GB, "
-                    f"Reserved {reserved_memory:.2f} GB "
-                    f"Max Reserved {max_reserved_memory:.2f} GB")
+        logger.info(
+            f"Rank {self.rank} Memory Stats: "
+            f"Allocated {allocated_memory:.2f} GB, "
+            f"Max Allocated {max_allocated_memory:.2f} GB, "
+            f"Reserved {reserved_memory:.2f} GB "
+            f"Max Reserved {max_reserved_memory:.2f} GB"
+        )
 
 
 def main(args: argparse.Namespace):
@@ -256,7 +276,7 @@ def main(args: argparse.Namespace):
     # build zmq client
     tcp_client = None
     if model_config.flexstore_port is not None:
-        tcp_client = ZMQClient('localhost', model_config.flexstore_port)
+        tcp_client = ZMQClient("localhost", model_config.flexstore_port)
         local_rank = int(os.getenv("LOCAL_RANK", "0"))
         tcp_client.send_pyobj(["get_rank", local_rank])
         rank = tcp_client.recv_pyobj()
@@ -266,35 +286,30 @@ def main(args: argparse.Namespace):
 
     tp_rank = parallel_state.get_tensor_model_parallel_rank()
     stage_id = parallel_state.get_pipeline_model_parallel_rank()
-    llm_runtime = LLMRuntime(model_id,
-                             model_name,
-                             tp_rank,
-                             stage_id,
-                             *engine_configs,
-                             tcp_client=tcp_client,
-                             runtime_profile=args.runtime_profile)
+    llm_runtime = LLMRuntime(
+        model_id,
+        model_name,
+        tp_rank,
+        stage_id,
+        *engine_configs,
+        tcp_client=tcp_client,
+        runtime_profile=args.runtime_profile,
+    )
 
-    # workload = Workload.from_workload_file(engine_args.workload_file)
-    workload = None
-    if engine_args.split_by_model is not None and workload is not None:
-        workload = workload.split_by_models(
-            engine_args.split_by_model.split(","))
-
-    muxserve_engine = MuxServeEngine(llm_runtime, workload,
-                                 engine_args.mps_percentage,
-                                 engine_args.is_prefill)
+    muxserve_engine = MuxServeEngine(
+        llm_runtime, engine_args.mps_percentage, engine_args.is_prefill
+    )
     # Start the engine loop.
     muxserve_engine.serve()
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='MuxServe Runtime Server')
-    parser.add_argument("--model-id",
-                        type=int,
-                        default=0,
-                        help="The index of served model.")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="MuxServe Runtime Server")
+    parser.add_argument(
+        "--model-id", type=int, default=0, help="The index of served model."
+    )
     parser.add_argument("--local-rank", type=int)
-    parser.add_argument('--runtime-profile', action='store_true')
+    parser.add_argument("--runtime-profile", action="store_true")
     parser = EngineArgs.add_cli_args(parser)
     args = parser.parse_args()
 
